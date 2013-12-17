5c3ae9355a969df0046e95f0f3b3fbb230385d88
Lars Mueller <l.mueller@datenspiel.com>
2013-12-17 10:53:31 +0100

    Recreates offer model

--- spec/lib/seatbelt/models/offer_spec.rb
+++ spec/lib/seatbelt/models/offer_spec.rb
@@ -1,179 +1,66 @@
 require 'spec_helper'
 
-describe Seatbelt::Models::Offer do
+describe ::Seatbelt::Models::Offer do
+  
+  subject { Seatbelt::Models::Offer.new }
 
-  #it_behaves_like "ApiClass"
-
-  describe "attributes" do
-
-    it "includes #price" do
-      expect(subject).to respond_to(:price)
+  %w( departure_date
+      days
+      operator
+      hotel_gid
+      hotel_goc
+      hotel_dlc
+      hotel_fid
+      hotel_ref
+      hotel_cat
+      hotel_name
+      city_gid
+      city_name
+      region_name
+      destination_name
+      departure_airport_code
+      price
+      on_price
+      bf_price
+      hb_price
+      fb_price
+      ai_price
+      entire_price
+      on_entire_price
+      bf_entire_price
+      hb_entire_price
+      fb_entire_price
+      ai_entire_price
+      distance_to_beach
+      water_temperature
+      air_temperature
+      hotel_hcid
+      hotel_reviews
+      hotel_rating
+      hotel_recommendation_rate ).each do |attr_name|
+  
+    it "responds to :#{attr_name}" do 
+      expect(subject).to respond_to attr_name
     end
-
-    it "includes #lcode" do
-      expect(subject).to respond_to(:lcode)
-    end
-
-    it "includes #lname" do
-      expect(subject).to respond_to(:lname)
-    end
-
-    it "includes #gidd" do
-      expect(subject).to respond_to(:gidd)
-    end
-
-    it "includes #travel_days" do
-      expect(subject).to respond_to(:travel_days)
-    end
-
-    it "includes #catering" do
-      expect(subject).to respond_to(:catering)
-    end
-
-    it "includes #room_type" do
-      expect(subject).to respond_to(:room_type)
-    end
-
-    it "includes #air_temperature" do
-      expect(subject).to respond_to(:air_temperature)
-    end
-
-    it "includes #water_temperature" do
-      expect(subject).to respond_to(:water_temperature)
-    end
-
-    it "includes #flight_duration" do
-      expect(subject).to respond_to(:flight_duration)
-    end
-
-    it "includes #rating" do
-      expect(subject).to respond_to(:rating)
-    end
-
-    it "includes #rating_count" do
-      expect(subject).to respond_to(:rating_count)
-    end
-
-    describe "#price" do
-
-      before{ subject.price = "762" }
-
-      it "is a Float" do
-        expect(subject.price).to be_instance_of Float
-      end
-
-    end
-
-    describe "#lcode" do
-
-      before{ subject.lcode = "PMI" }
-
-      it "is a String" do
-        expect(subject.lcode).to be_instance_of String
-      end
-
-    end
-
-    describe "#lname" do
-
-      before{ subject.lname = "Spanien" }
-
-      it "is a String" do
-        expect(subject.lname).to be_instance_of String
-      end
-
-    end
-
-    describe "#lname" do
-
-      before{ subject.gidd = "67356 7729" }
-
-      it "is a String" do
-        expect(subject.gidd).to be_instance_of String
-      end
-
-    end
-
-    describe "#travel_days" do
-
-      before{ subject.travel_days = "12" }
-
-      it "is a Integer" do
-        expect(subject.travel_days).to be_instance_of Fixnum
-      end
-
-    end
-
-    describe "#catering" do
-
-      before{ subject.catering = "Halbpension" }
-
-      it "is a String" do
-        expect(subject.catering).to be_instance_of String
-      end
-
-    end
-
-    describe "#catering" do
-
-      before{ subject.room_type = "Studio" }
-
-      it "is a String" do
-        expect(subject.room_type).to be_instance_of String
-      end
-
-    end
-
-    describe "#air_temperature" do
-
-      before{ subject.air_temperature = "34.4" }
-
-      it "is a Float" do
-        expect(subject.air_temperature).to be_instance_of Float
-      end
-
-    end
-
-    describe "#water_temperature" do
-
-      before{ subject.water_temperature = "23.1" }
-
-      it "is a Float" do
-        expect(subject.water_temperature).to be_instance_of Float
-      end
-
-    end
-
-    describe "#flight_duration" do
-
-      before{ subject.flight_duration = "11" }
-
-      it "is a Integer" do
-        expect(subject.flight_duration).to be_instance_of Fixnum
-      end
-
-    end
-
-    describe "#rating" do
-
-      before{ subject.rating = "5" }
-
-      it "is a Integer" do
-        expect(subject.rating).to be_instance_of Fixnum
-      end
-
-    end
-
-    describe "#rating_count" do
-
-      before{ subject.rating_count = "11" }
-
-      it "is a Integer" do
-        expect(subject.rating_count).to be_instance_of Fixnum
-      end
-
-    end
-
 
   end
+
+  describe "#lowest_price" do
+
+    it "returns the lowest price of available prices" do
+      subject.attributes = { on_price: 100, bf_price: 120, ai_price: 180 }
+      expect(subject.lowest_price).to eq 100
+    end
+  
+  end
+
+  describe "#lowest_entire_price" do
+
+    it "returns the lowest entire price of available entire prices" do
+      subject.attributes = { bf_entire_price: 240, ai_entire_price: 310 }
+      expect(subject.lowest_entire_price).to eq 240
+    end
+  
+  end
+
 end
